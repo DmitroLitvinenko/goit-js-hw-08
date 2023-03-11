2ca3267095218bdbbe4dc581d1748443f8c41db3
sides1 <dima.litvinenko1990@gmail.com>
2023-03-11 15:40:00 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+
+
+const imgElements = galleryItems.map( ({original, preview, description} )  => {
+    return `
+    <li>
+    <a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>
+    </li>
+    
+  </div>`}).join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', imgElements);
+
+
+galleryContainer.addEventListener( 'click', onGalleryElementClick)
+
+function onGalleryElementClick(event) {
+    event.preventDefault();
+    
+    if(event.target.nodeName !== 'IMG') {
+        return;
+}
+};
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt',
+captionDelay: 250,
+captionPosition: 'bottom',
+captionType: 'attr', });
 
 console.log(galleryItems);
