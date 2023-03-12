904d6cc73f67ea14ec7da063e7e86e9b477d8759
sides1 <dima.litvinenko1990@gmail.com>
2023-03-12 18:29:58 +0200

    complete task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from "lodash.throttle";
 
+
+const STORAGE_KEY = 'feedback-form-state'
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea')
+}
+
+loadFormState();
+
+
+function saveFormState () {
+    const state = {
+        email: refs.input.value,
+        message: refs.textarea.value,
+        };
+        
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+    };
+
+function loadFormState () {
+
+    const stateString = localStorage.getItem(STORAGE_KEY);
+    const parsedString = JSON.parse(stateString);
+    if(parsedString) {
+        refs.input.value = parsedString.email;
+        refs.textarea.value = parsedString.message;
+    }
+    };
+
+
+function onFormSubmit (event)  {
+    event.preventDefault();
+    
+
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+
+};
+
+
+refs.form.addEventListener('input', throttle(saveFormState, 500));
+refs.form.addEventListener('submit', onFormSubmit);
