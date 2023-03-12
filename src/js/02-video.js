62800a61e72c10cd85e173d549459df168aeab3f
sides1 <dima.litvinenko1990@gmail.com>
2023-03-12 16:23:28 +0200

    work in task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    player.getCurrentTime({
+        duration: 61.857,
+        percent: 0.049,
+        seconds: 3.034});
+
 
