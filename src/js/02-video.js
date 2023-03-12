7760ce5afa3c837481d9bb262787ff82455d892f
sides1 <dima.litvinenko1990@gmail.com>
2023-03-12 16:44:36 +0200

    task-02 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,17 +5,12 @@ import throttle from 'lodash.throttle';
     const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
 
-    player.on('timeupdate', function() {
-        console.log('played the video!');
-    });
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+    player.on('timeupdate', throttle(onPlay, 1000));
 
-    player.getCurrentTime({
-        duration: 61.857,
-        percent: 0.049,
-        seconds: 3.034});
-
-
+    function onPlay({ seconds }) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    }
+    
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+    
