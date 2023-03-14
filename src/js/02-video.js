a455ba8ab24a33337551a4d796119548ea502e2b
sides1 <dima.litvinenko1990@gmail.com>
2023-03-15 00:45:59 +0200

    fix task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,19 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
 
-    const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+player.on('timeupdate', throttle(onPlay, 1000));
 
+function onPlay({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
 
-    player.on('timeupdate', throttle(onPlay, 1000));
-
-    function onPlay({ seconds }) {
-      localStorage.setItem('videoplayer-current-time', seconds);
-    }
-    
-    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-    
+window.addEventListener('load', () => {
+  const savedTime = localStorage.getItem(STORAGE_KEY);
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+});
