2079dadb2c19bdda18e114eeced50c62388ac596
sides1 <dima.litvinenko1990@gmail.com>
2023-03-15 00:33:26 +0200

    fix task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,45 +1,45 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-
-const STORAGE_KEY = 'feedback-form-state'
+const STORAGE_KEY = 'feedback-form-state';
 const refs = {
-    form: document.querySelector('.feedback-form'),
-    input: document.querySelector('input'),
-    textarea: document.querySelector('textarea')
-}
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
 
 loadFormState();
 
+function saveFormState() {
+  const state = {
+    email: refs.input.value,
+    message: refs.textarea.value,
+  };
 
-function saveFormState () {
-    const state = {
-        email: refs.input.value,
-        message: refs.textarea.value,
-        };
-        
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
-    };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+}
 
-function loadFormState () {
+function loadFormState() {
+  const stateString = localStorage.getItem(STORAGE_KEY);
+  const parsedString = JSON.parse(stateString);
+  if (parsedString) {
+    refs.input.value = parsedString.email;
+    refs.textarea.value = parsedString.message;
+  }
+}
 
-    const stateString = localStorage.getItem(STORAGE_KEY);
-    const parsedString = JSON.parse(stateString);
-    if(parsedString) {
-        refs.input.value = parsedString.email;
-        refs.textarea.value = parsedString.message;
-    }
-    };
+function onFormSubmit(event) {
+  event.preventDefault();
 
+  const state = {
+    email: refs.input.value,
+    message: refs.textarea.value,
+  };
 
-function onFormSubmit (event)  {
-    event.preventDefault();
-    
+  event.currentTarget.reset();
+  console.log(state);
 
-    event.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-
-};
-
+  localStorage.removeItem(STORAGE_KEY);
+}
 
 refs.form.addEventListener('input', throttle(saveFormState, 500));
 refs.form.addEventListener('submit', onFormSubmit);
